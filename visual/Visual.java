080a84745419a6893f338a9a433c077f41d9b631
Budanew <rileydan2004@gmail.com>
2025-09-27 13:21:28 -0400

    frame

--- visual/Visual.java
+++ visual/Visual.java
@@ -1,7 +1,18 @@
 package visual;
 
-import java.awt.BorderLayout;
+import javax.swing.JFrame;
 
+public class Visual
+{
+    public static void main(String[] args) 
+    {
+        JFrame frame = new JFrame("Task Master");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    // private JFrame frame;
+}
 
 
 
