543503a948a1ade2714a9e56b63c3a831132b5eb
Budanew <rileydan2004@gmail.com>
2025-09-27 14:11:31 -0400

    input task panel

--- visual/InputTask.java
+++ visual/InputTask.java
@@ -1,9 +1,25 @@
 package visual;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class InputTask extends JPanel{
-    public InputTask() {
-        
+
+    private JLabel label;
+
+    private JButton button;
+
+    private JTextField inputField;
+
+    public InputTask() 
+    {
+        this.setLayout(new FlowLayout());
+        this.label = new JLabel("Write a Task");
+        this.button = new JButton("Enter");
+        this.inputField = new JTextField(10);
+        this.add(label);
+        this.add(button);
+        this.add(inputField);
     }
 }
