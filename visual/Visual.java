50c65408141c44eef0f1dabed4da81e80ca768e2
Budanew <rileydan2004@gmail.com>
2025-09-27 13:42:24 -0400

    visual works

--- visual/Visual.java
+++ visual/Visual.java
@@ -2,16 +2,27 @@ package visual;
 
 import javax.swing.JFrame;
 
-public class Visual
+public class Visual extends JFrame
 {
-    public static void main(String[] args) 
+    private JFrame frame;
+    private int timerInput; 
+    
+
+
+    public Visual()
     {
-        JFrame frame = new JFrame("Task Master");
+        this.timerInput = 0;
+        this.frame = makeWindow();
+    }
+
+    private JFrame makeWindow()
+    {
+        JFrame frame = new JFrame("Task_Master");
         frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        return frame;
     }
-    // private JFrame frame;
 }
 
 
